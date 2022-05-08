83bd8ed328ab717f320429cefe7ea021a848852b
christmc <christophermins@gmail.com>
2022-05-08 17:47:38 +0800

    sqlite support

--- src/workflow_database.rs
+++ src/workflow_database.rs
@@ -7,16 +7,21 @@ impl AlfredWorkflow {
         Connection::open(path).unwrap()
     }
 
-    pub fn query<P: Params, F, T>(db: &Connection, sql: &str, param: P, map_fun: F) -> MappedRows<F>
+    pub fn query<P: Params, F, T>(db: &Connection, sql: &str, param: P, map_fun: F) -> Vec<T>
         where
             P: Params,
-            F: FnMut(&Row<'_>) -> T,
+            F: FnMut(&Row<'_>) -> Result<T>,
     {
-        let stmt = db.prepare(sql).unwrap();
-        stmt.query_map(param, map_fun).unwrap()
+        let mut stmt = db.prepare(sql).unwrap();
+        let rows = stmt.query_map(param, map_fun).unwrap();
+        let mut rts = Vec::new();
+        for row in rows {
+            rts.push(row.unwrap())
+        }
+        rts
     }
 
     pub fn close(db: Connection) {
-        db.close()
+        db.close().unwrap();
     }
 }
